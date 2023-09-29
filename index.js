00b61b0b49dba899d362585800643e6da01cb3ca
brevre <brendamakena09@gmail.com>
2023-09-29 07:25:16 +0200

    completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  
+
+  const leastFavoriteCustomer = 'someone';
+
+  function changeLeastFavoriteCustomer(newLeastFavorite) {
+    // This will throw an error because you can't reassign a constant
+    leastFavoriteCustomer = newLeastFavorite;
+  }
+  
+  
